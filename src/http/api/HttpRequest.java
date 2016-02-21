fdd13728a7fd4514c5a30768fb5dd90c2a26a2f0
wutao <82wutao@gmail.com>
2016-02-21 23:31:31 +0800

    head

--- src/http/api/HttpRequest.java
+++ src/http/api/HttpRequest.java
@@ -4,22 +4,259 @@ import java.io.IOException;
 import java.util.Map;
 
 public interface HttpRequest {
-	public static final String Head_METHOD_Request = "Method";
-	public static final String Head_Uri_Request = "Uri";
-	public static final String Head_HttpVersion_Request = "Version";
-	
-	public static final String Head_Accept_Request = "Accept";
-	public static final String Head_AcceptLanguage_Request = "Accept-Language";
-	public static final String Head_AcceptEcoding_Request = "Accept-Encoding";
-	public static final String Head_Connection_Request = "Connection";
-	public static final String Head_ContentLength_Request = "Content-Length";
-	public static final String Head_ContentType_Request = "Content-Type";
-	public static final String Head_Cookie_Request = "Cookie";
-	public static final String Head_DNT_Request = "DNT";
-	public static final String Head_Host_Request = "Host";
-	public static final String Head_Referer_Request = "Referer";
-	public static final String Head_UserAgent_Request = "User-Agent";
-	
+	// public static final String Head_METHOD_Request = "Method";
+	// public static final String Head_Uri_Request = "Uri";
+	// public static final String Head_HttpVersion_Request = "Version";
+	//
+	// public static final String Head_Accept_Request = "Accept";
+	// public static final String Head_AcceptLanguage_Request =
+	// "Accept-Language";
+	// public static final String Head_AcceptEcoding_Request =
+	// "Accept-Encoding";
+	// public static final String Head_Connection_Request = "Connection";
+	// public static final String Head_ContentLength_Request = "Content-Length";
+	// public static final String Head_ContentType_Request = "Content-Type";
+	// public static final String Head_Cookie_Request = "Cookie";
+	// public static final String Head_DNT_Request = "DNT";
+	// public static final String Head_Host_Request = "Host";
+	// public static final String Head_Referer_Request = "Referer";
+	// public static final String Head_UserAgent_Request = "User-Agent";
+	/**
+	 * 能够接受的回应内容类型（Content-Types）。参见内容协商。 Accept: text/plain Permanent
+	 */
+	public static final String Accept = "Accept";
+	/**
+	 * 能够接受的字符集 Accept-Charset: utf-8 Permanent
+	 */
+	public static final String Accept_Charset = "Accept-Charset";
+	/**
+	 * 能够接受的编码方式列表。参考 超文本传输协议压缩 。 Accept-Encoding: gzip, deflate Permanent
+	 */
+	public static final String Accept_Encoding = "Accept-Encoding";
+	/**
+	 * 能够接受的回应内容的自然语言列表。参考 内容协商 。 Accept-Language: en-US Permanent
+	 */
+	public static final String Accept_Language = "Accept-Language";
+	/**
+	 * 能够接受的按照时间来表示的版本 Accept-Datetime: Thu, 31 May 2007 20:35:00 GMT
+	 * Provisional
+	 */
+	public static final String Accept_Datetime = "Accept-Datetime";
+	/**
+	 * 用于超文本传输协议的认证的认证信息 Authorization: Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==
+	 * Permanent
+	 */
+	public static final String Authorization = "Authorization";
+	/**
+	 * 用来指定在这次的请求/回复链中的所有缓存机制 都必须 遵守的指令 Cache-Control: no-cache Permanent
+	 */
+	public static final String Cache_Control = "Cache-Control";
+	/**
+	 * 该浏览器想要优先使用的连接类型 Connection: keep-alive Connection: Upgrade Permanent
+	 */
+	public static final String Connection = "Connection";
+	/**
+	 * 之前由服务器通过 Set- Cookie （下文详述）发送的一个 超文本传输协议Cookie Cookie: $Version=1;
+	 * Skin=new; Permanent: standard
+	 */
+	public static final String Cookie = "Cookie";
+	/**
+	 * 以 八位字节数组 （8位的字节）表示的请求体的长度 Content-Length: 348 Permanent
+	 */
+	public static final String Content_Length = "Content-Length";
+	/**
+	 * 请求体的内容的二进制 MD5 散列值，以 Base64 编码的结果 Content-MD5: Q2hlY2sgSW50ZWdyaXR5IQ==
+	 * Obsolete[4]
+	 */
+	public static final String Content_MD5 = "Content-MD5";
+	/**
+	 * 请求体的 多媒体类型 （用于POST和PUT请求中） Content-Type:
+	 * application/x-www-form-urlencoded Permanent
+	 */
+	public static final String Content_Type = "Content-Type";
+	/**
+	 * 发送该消息的日期和时间(按照 RFC 7231 中定义的"超文本传输协议日期"格式来发送) Date: Tue, 15 Nov 1994
+	 * 08:12:31 GMT Permanent
+	 */
+	public static final String Date = "Date";
+	/**
+	 * 表明客户端要求服务器做出特定的行为 Expect: 100-continue Permanent
+	 */
+	public static final String Expect = "Expect";
+	/**
+	 * 发起此请求的用户的邮件地址 From: user@example.com Permanent
+	 */
+	public static final String From = "From";
+	/**
+	 * 服务器的域名(用于虚拟主机)，以及服务器所监听的传输控制协议端口号。 如果所请求的端口是对应的服务的标准端口，则端口号可被省略。
+	 * 自超文件传输协议版本1.1（HTTP/1.1）开始便是必需字段。 Host: en.wikipedia.org:80 Host:
+	 * en.wikipedia.org Permanent
+	 */
+	public static final String Host = "Host";
+	/**
+	 * 仅当客户端提供的实体与服务器上对应的实体相匹配时，才进行对应的操作。 主要作用时，用作像 PUT
+	 * 这样的方法中，仅当从用户上次更新某个资源以来，该资源未被修改的情况下，才更新该资源。 If-Match:
+	 * "737060cd8c284d8af7ad3082f209582d" Permanent
+	 */
+	public static final String If_Match = "If-Match";
+	/**
+	 * 允许在对应的内容未被修改的情况下返回304未修改（ 304 Not Modified ） If-Modified-Since: Sat, 29
+	 * Oct 1994 19:43:31 GMT Permanent
+	 */
+	public static final String If_Modified_Since = "If-Modified-Since";
+	/**
+	 * 允许在对应的内容未被修改的情况下返回304未修改（ 304 Not Modified ），参考超文本传输协议的实体标记
+	 * If-None-Match: "737060cd8c284d8af7ad3082f209582d" Permanent
+	 */
+	public static final String If_None_Match = "If-None-Match";
+	/**
+	 * 如果该实体未被修改过，则向我发送我所缺少的那一个或多个部分；否则，发送整个新的实体 If-Range:
+	 * "737060cd8c284d8af7ad3082f209582d" Permanent
+	 */
+	public static final String If_Range = "If-Range";
+	/**
+	 * 仅当该实体自某个特定时间已来未被修改的情况下，才发送回应。 If-Unmodified-Since: Sat, 29 Oct 1994
+	 * 19:43:31 GMT Permanent
+	 */
+	public static final String If_Unmodified_Since = "If-Unmodified-Since";
+	/**
+	 * 限制该消息可被代理及网关转发的次数。 Max-Forwards: 10 Permanent
+	 */
+	public static final String Max_Forwards = "Max-Forwards";
+	/**
+	 * 发起一个针对 跨来源资源共享
+	 * 的请求（要求服务器在回应中加入一个‘访问控制-允许来源’（'Access-Control-Allow-Origin'）字段）。 Origin:
+	 * http://www.example-social-network.com Permanent: standard
+	 */
+	public static final String Origin = "Origin";
+	/**
+	 * 与具体的实现相关，这些字段可能在请求/回应链中的任何时候产生 多种效果。 Pragma: no-cache Permanent
+	 */
+	public static final String Pragma = "Pragma";
+	/**
+	 * 用来向代理进行认证的认证信息。 Proxy-Authorization: Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==
+	 * Permanent
+	 */
+	public static final String Proxy_Authorization = "Proxy-Authorization";
+	/**
+	 * 仅请求某个实体的一部分。字节偏移以0开始。参考 字节服务 。 Range: bytes=500-999 Permanent
+	 */
+	public static final String Range = "Range";
+	/**
+	 * 表示浏览器所访问的前一个页面，正是那个页面上的某个链接将浏览器带到了当前所请求的这个页面。 (“引导者”（“referrer”）这个单词，在RFC
+	 * 中被拼错了，因此在大部分的软件实现中也拼错了，以至于，错误的拼法成为了标准的用法，还被当成了正确的术语) Referer:
+	 * http://en.wikipedia.org/wiki/Main_Page Permanent
+	 */
+	public static final String Referer = "Referer";
+	/**
+	 * 浏览器预期接受的传输编码方式：可使用回应协议头Transfer-Encoding 字段中的那些值，另外还有一个值可用，"trailers"（与
+	 * " 分 块 "传输方式相关），用来表明，浏览器希望在最后一个尺寸为0的块之后还接收到一些额外的字段。 TE: trailers, deflate
+	 * Permanent
+	 */
+	public static final String TE = "TE";
+	/**
+	 * 浏览器的浏览器身份标识字符串 User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:12.0)
+	 * Gecko/20100101 Firefox/21.0 Permanent
+	 */
+	public static final String User_Agent = "User-Agent";
+	/**
+	 * 要求服务器升级到另一个协议。 Upgrade: HTTP/2.0, SHTTP/1.3, IRC/6.9, RTA/x11 Permanent
+	 */
+	public static final String Upgrade = "Upgrade";
+	/**
+	 * 向服务器告知，这个请求是由哪些代理发出的。 Via: 1.0 fred, 1.1 example.com (Apache/1.1)
+	 * Permanent
+	 */
+	public static final String Via = "Via";
+	/**
+	 * 一个一般性的警告，告知，在实体内容体中可能存在错误。 Warning: 199 Miscellaneous warning Permanent
+	 */
+	public static final String Warning = "Warning";
+	/**
+	 * 主要用于标识 Ajax 及可扩展标记语言 请求。大部分的Javascript框架 会发送这个字段，且将其值设置为 XMLHttpRequest
+	 * X-Requested-With: XMLHttpRequest
+	 */
+	public static final String X_Requested_With = "X-Requested-With";
+	/**
+	 * 请求某个网页应用程序停止跟踪某个用户。在火狐浏览器中，相当于X-Do-Not-Track协议头字段(自 火狐4.0 测试（Beta）
+	 * 11版开始支持)。 Safari 和 Internet Explorer 9 也支持这个字段。在2011 年三月7
+	 * 日，有人向互联网工程任务组提交了一个草案。 万维网协会 的跟踪保护工作组正在就此制作一项规范。 DNT: 1 (Do Not Track
+	 * Enabled) DNT: 0 (Do Not Track Disabled)
+	 */
+	public static final String DNT = "DNT";
+	/**
+	 * 一个事实标准 ，用于标识某个通过超文本传输协议代理或负载均衡连接到某个网页服务器的客户端的原始互联网地址 X-Forwarded-For:
+	 * client1, proxy1, proxy2 X-Forwarded-For: 129.78.138.66, 129.78.64.103
+	 * 
+	 */
+	public static final String X_Forwarded_For = "X-Forwarded-For";
+	/**
+	 * a de facto standard for identifying the original host requested by the
+	 * client in the Host HTTP request header, since the host name and/or port
+	 * of the reverse proxy (load balancer) may differ from the origin server
+	 * handling the request. X-Forwarded-Host: en.wikipedia.org:80
+	 * X-Forwarded-Host: en.wikipedia.org
+	 * 
+	 */
+	public static final String X_Forwarded_Host = "X-Forwarded-Host";
+	/**
+	 * 一个事实标准
+	 * 用于标识某个超文本传输协议请求最初所使用的协议，因为，在反向代理(负载均衡)上，即使最初发往该反向代理的请求类型是安全的超文本传输协议（HTTPS
+	 * ）， 该反向代理也仍然可能会使用超文本传输协议（HTTP）来与网页服务器通信。
+	 * 谷歌客户端在与谷歌服务器通信时会使用该协议头的一个替代形式（X-ProxyUser-Ip）。 X-Forwarded-Proto: https
+	 * 
+	 */
+	public static final String X_Forwarded_Proto = "X-Forwarded-Proto";
+	/**
+	 * Non-standard header field used by Microsoft applications and
+	 * load-balancers Front-End-Https: on
+	 * 
+	 */
+	public static final String Front_End_Https = "Front-End-Https";
+	/**
+	 * 请求某个网页应用程序使用该协议头字段中指定的方法（一般是PUT或DELETE）来覆盖掉在请求中所指定的方法（一般是POST）。
+	 * 当某个浏览器或防火墙阻止直接发送PUT 或DELETE
+	 * 方法时（注意，这可能是因为软件中的某个漏洞，因而需要修复，也可能是因为某个配置选项就是如此要求的，因而不应当设法绕过），可使用这种方式。
+	 * X-HTTP-Method-Override: DELETE
+	 * 
+	 */
+	public static final String X_Http_Method_Override = "X-Http-Method-Override";
+	/**
+	 * Allows easier parsing of the MakeModel/Firmware that is usually found in
+	 * the User-Agent String of AT&T Devices X-Att-Deviceid: GT-P7320/P7320XXLPG
+	 * 
+	 */
+	public static final String X_ATT_DeviceId = "X-ATT-DeviceId";
+	/**
+	 * Links to an XML file on the Internet with a full description and details
+	 * about the device currently connecting. In the example to the right is an
+	 * XML file for an AT&T Samsung Galaxy S2. x-wap-profile:
+	 * http://wap.samsungmobile.com/uaprof/SGH-I777.xml
+	 * 
+	 */
+	public static final String X_Wap_Profile = "X-Wap-Profile";
+	/**
+	 * 因为对超文本传输协议规范的误解而实现的一个协议头。
+	 * 因为早期超文本传输协议版本实现中的错误而出现。与标准的连接（Connection）字段的功能完全相同。 Proxy-Connection:
+	 * keep-alive
+	 * 
+	 */
+	public static final String Proxy_Connection = "Proxy-Connection";
+	/**
+	 * Server-side deep packet insertion of a unique ID identifying customers of
+	 * Verizon Wireless; also known as "perma-cookie" or "supercookie" X-UIDH:
+	 * ...
+	 * 
+	 */
+	public static final String X_UIDH = "X-UIDH";
+	/**
+	 * Used to prevent cross-site request forgery. Alternative header names are:
+	 * X-CSRFToken[21] and X-XSRF-TOKEN[22] X-Csrf-Token:
+	 * i8XNjC4b8KVok4uw5RftR38Wgp2BFwql
+	 * 
+	 */
+	public static final String X_Csrf_Token = "X-Csrf-Token";
+
 	public Map<String, String> getRequestHeads();
 
 	public String getRequestHead(String key);
@@ -36,12 +273,11 @@ public interface HttpRequest {
 
 	public Cookie[] getCookies();
 
-	public int readFromBody(byte[] buffer)
-			throws IOException;
+	public int readFromBody(byte[] buffer) throws IOException;
 
 	public String dumpHead();
 
 	public String dumpBody();
-	
+
 	public MultiPartForm getMultiPartForm();
 }
--- src/http/api/HttpResponse.java
+++ src/http/api/HttpResponse.java
@@ -6,37 +6,288 @@ import java.io.OutputStream;
 import java.util.Map;
 
 public interface HttpResponse {
-	public static final String Head_AcceptRanges_Response="Accept-Ranges";
-	public static final String Head_Age_Response="Age";
-	public static final String Head_CacheControl_Response="Cache-Control";
-	public static final String Head_Connection_Response="Connection";
-	public static final String Head_ContentEncoding_Response="Content-Encoding";
-	public static final String Head_ContentLength_Response="Content-Length";
-	public static final String Head_ContentType_Response="Content-Type";
-	
-	public static final String Head_Date_Response="Date";
-	public static final String Head_Expires_Response="Expires";
-	public static final String Head_Server_Response="Server";
-	public static final String Head_SetCookie_Response="Set-Cookie";
-	public static final String Head_Vary_Response="Vary";
-	public static final String Head_XCache_Response="X-Cache";
+	// public static final String Head_AcceptRanges_Response="Accept-Ranges";
+	// public static final String Head_Age_Response="Age";
+	// public static final String Head_CacheControl_Response="Cache-Control";
+	// public static final String Head_Connection_Response="Connection";
+	// public static final String
+	// Head_ContentEncoding_Response="Content-Encoding";
+	// public static final String Head_ContentLength_Response="Content-Length";
+	// public static final String Head_ContentType_Response="Content-Type";
+	//
+	// public static final String Head_Date_Response="Date";
+	// public static final String Head_Expires_Response="Expires";
+	// public static final String Head_Server_Response="Server";
+	// public static final String Head_SetCookie_Response="Set-Cookie";
+	// public static final String Head_Vary_Response="Vary";
+	// public static final String Head_XCache_Response="X-Cache";
 
+	/**
+	 * 指定哪些网站可参与到 跨来源资源共享 过程中 Access-Control-Allow-Origin: * Provisional
+	 */
+	public static final String Access_Control_Allow_Origin = "Access-Control-Allow-Origin";
+	/**
+	 * Specifies which patch document formats this server supports Accept-Patch:
+	 * text/example;charset=utf-8 Permanent
+	 */
+	public static final String Accept_Patch = "Accept-Patch";
+	/**
+	 * 这个服务器支持哪些种类的部分内容范围 Accept-Ranges: bytes Permanent
+	 */
+	public static final String Accept_Ranges = "Accept-Ranges";
+	/**
+	 * 这个对象在 代理缓存 中存在的时间，以秒为单位 Age: 12 Permanent
+	 */
+	public static final String Age = "Age";
+	/**
+	 * 对于特定资源有效的动作。针对405不允许该方法（ 405 Method not allowed ）而使用 Allow: GET, HEAD
+	 * Permanent
+	 */
+	public static final String Allow = "Allow";
+	/**
+	 * 向从服务器直到客户端在内的所有缓存机制告知，它们是否可以缓存这个对象。其单位为秒 Cache-Control: max-age=3600
+	 * Permanent
+	 */
+	public static final String Cache_Control = "Cache-Control";
+	/**
+	 * 针对该连接所预期的选项 Connection: close Permanent
+	 */
+	public static final String Connection = "Connection";
 
+	/**
+	 * An opportunity to raise a "File Download" dialogue box for a known MIME
+	 * type with binary format or suggest a filename for dynamic content. Quotes
+	 * are necessary with special characters. Content-Disposition: attachment;
+	 * filename="fname.ext" Permanent
+	 */
+	public static final String Content_Disposition = "Content-Disposition";
+	/**
+	 * 在数据上使用的编码类型。参考 超文本传输协议压缩 。 Content-Encoding: gzip Permanent
+	 */
+	public static final String Content_Encoding = "Content-Encoding";
+	/**
+	 * 内容所使用的语言 Content-Language: da Permanent
+	 */
+	public static final String Content_Language = "Content-Language";
+	/**
+	 * 回应消息体的长度，以 字节 （8位为一字节）为单位 Content-Length: 348 Permanent
+	 */
+	public static final String Content_Length = "Content-Length";
+	/**
+	 * 所返回的数据的一个候选位置 Content-Location: /index.htm Permanent
+	 */
+	public static final String Content_Location = "Content-Location";
+	/**
+	 * 回应内容的二进制 MD5 散列，以 Base64 方式编码 Content-MD5: Q2hlY2sgSW50ZWdyaXR5IQ==
+	 * Obsolete[26]
+	 * 
+	 */
+	public static final String Content_MD5 = "Content-MD5";
+	/**
+	 * 这条部分消息是属于某条完整消息的哪个部分 Content-Range: bytes 21010-47021/47022 Permanent
+	 */
+	public static final String Content_Range = "Content-Range";
+	/**
+	 * 当前内容的MIME类型 Content-Type: text/html; charset=utf-8 Permanent
+	 */
+	public static final String Content_Type = "Content-Type";
+	/**
+	 * 此条消息被发送时的日期和时间(按照 RFC 7231 中定义的“超文本传输协议日期”格式来表示) Date: Tue, 15 Nov 1994
+	 * 08:12:31 GMT Permanent
+	 */
+	public static final String Date = "Date";
+	/**
+	 * 对于某个资源的某个特定版本的一个标识符，通常是一个 消息散列 ETag: "737060cd8c284d8af7ad3082f209582d"
+	 * Permanent
+	 */
+	public static final String ETag = "ETag";
+	/**
+	 * 指定一个日期/时间，超过该时间则认为此回应已经过期 Expires: Thu, 01 Dec 1994 16:00:00 GMT
+	 * Permanent: standard
+	 */
+	public static final String Expires = "Expires";
+	/**
+	 * 所请求的对象的最后修改日期(按照 RFC 7231 中定义的“超文本传输协议日期”格式来表示) Last-Modified: Tue, 15
+	 * Nov 1994 12:45:26 GMT Permanent
+	 */
+	public static final String Last_Modified = "Last-Modified";
+	/**
+	 * 用来表达与另一个资源之间的类型关系，此处所说的类型关系是在 RFC 5988 中定义的 Link: </feed>;
+	 * rel="alternate"[27] Permanent
+	 */
+	public static final String Link = "Link";
+	/**
+	 * 用来 进行 重定向 ，或者在创建了某个新资源时使用。 Location:
+	 * http://www.w3.org/pub/WWW/People.html Permanent
+	 */
+	public static final String Location = "Location";
+	/**
+	 * This field is supposed to set P3P policy, in the form of
+	 * P3P:CP="your_compact_policy". However, P3P did not take off,[28] most
+	 * browsers have never fully implemented it, a lot of websites set this
+	 * field with fake policy text, that was enough to fool browsers the
+	 * existence of P3P policy and grant permissions for third party cookies.
+	 * P3P: CP=
+	 * "This is not a P3P policy! See http://www.google.com/support/accounts/bin/answer.py?hl=en&answer=151657 for more info."
+	 * Permanent
+	 */
+	public static final String P3P = "P3P";
+	/**
+	 * 与具体的实现相关，这些字段可能在请求/回应链中的任何时候产生多种效果。 Pragma: no-cache Permanent
+	 */
+	public static final String Pragma = "Pragma";
+	/**
+	 * 要求在访问代理时提供身份认证信息。 Proxy-Authenticate: Basic Permanent
+	 */
+	public static final String Proxy_Authenticate = "Proxy-Authenticate";
+	/**
+	 * 用于缓解 中间人攻击 ，声明网站的认证用的 传输 层安全协议 证书的散列值 Public-Key-Pins: max-age=2592000;
+	 * pin-sha256="E9CZ9INDbd+2eRQozYqqbQ2yXLVKB9+xcprMF+44U1g="; Permanent
+	 */
+	public static final String Public_Key_Pins = "Public-Key-Pins";
+	/**
+	 * Used in redirection, or when a new resource has been created. This
+	 * refresh redirects after 5 seconds. Refresh: 5;
+	 * url=http://www.w3.org/pub/WWW/People.html Proprietary and non-standard: a
+	 * header extension introduced by Netscape and supported by most web
+	 * browsers.
+	 */
+	public static final String Refresh = "Refresh";
+	/**
+	 * 如果某个实体临时不可用，则，此协议头用来告知客户端日后重试。 其值可以是一个特定的时间段(以秒为单位)或一个超文本传输协议日期。
+	 * Retry-After: 120 Retry-After: Fri, 07 Nov 2014 23:59:59 GMT Permanent
+	 */
+	public static final String Retry_After = "Retry-After";
+	/**
+	 * 服务器的名字 Server: Apache/2.4.1 (Unix) Permanent
+	 */
+	public static final String Server = "Server";
+	/**
+	 * HTTP cookie Set-Cookie: UserID=JohnDoe; Max-Age=3600; Version=1
+	 * Permanent: standard
+	 */
+	public static final String Set_Cookie = "Set-Cookie";
+	/**
+	 * 通用网关接口 协议头字段，用来说明当前这个超文本传输协议回应的 状态 。
+	 * 普通的超文本传输协议回应，会使用单独的“状态行”（"Status-Line"）作为替代，这一点是在 RFC 7230 中定义的。 Status:
+	 * 200 OK Not listed as a registered field name
+	 */
+	public static final String Status = "Status";
 
-	
-	public void setResponseHead(String key, String value) ;
+	/**
+	 * A HSTS Policy informing the HTTP client how long to cache the HTTPS only
+	 * policy and whether this applies to subdomains. Strict-Transport-Security:
+	 * max-age=16070400; includeSubDomains Permanent: standard
+	 */
+	public static final String Strict_Transport_Security = "Strict-Transport-Security";
+	/**
+	 * The Trailer general field value indicates that the given set of header
+	 * fields is present in the trailer of a message encoded with chunked
+	 * transfer coding. Trailer: Max-Forwards Permanent
+	 */
+	public static final String Trailer = "Trailer";
+	/**
+	 * 用来将实体安全地传输给用户的编码形式。 当前定义 的方法 包括： 分 块
+	 * 、压缩（compress）、缩小（deflate）、压缩（gzip）、实体（identity）。 Transfer-Encoding:
+	 * chunked Permanent
+	 */
+	public static final String Transfer_Encoding = "Transfer-Encoding";
+	/**
+	 * 要求客户端升级到另一个协议。 Upgrade: HTTP/2.0, SHTTP/1.3, IRC/6.9, RTA/x11 Permanent
+	 */
+	public static final String Upgrade = "Upgrade";
+	/**
+	 * 告知下游的代理服务器，应当如何对未来的请求协议头进行匹配，以决定是否可使用已缓存的回应内容而不是重新从原始服务器请求新的内容。 Vary: *
+	 * Permanent
+	 */
+	public static final String Vary = "Vary";
+	/**
+	 * 告知代理服务器的客户端，当前回应是通过什么途径发送的。 Via: 1.0 fred, 1.1 example.com (Apache/1.1)
+	 * Permanent
+	 */
+	public static final String Via = "Via";
+	/**
+	 * 一般性的警告，告知在实体内容体中可能存在错误。 Warning: 199 Miscellaneous warning Permanent
+	 */
+	public static final String Warning = "Warning";
+	/**
+	 * 表明在请求获取这个实体时应当使用的认证模式。 WWW-Authenticate: Basic Permanent
+	 */
+	public static final String WWW_Authenticate = "WWW-Authenticate";
+	/**
+	 * Clickjacking protection: deny - no rendering within a frame, sameorigin -
+	 * no rendering if origin mismatch, allow-from - allow from specified
+	 * location, allowall - non-standard, allow from any location
+	 * X-Frame-Options: deny Obsolete[33]
+	 */
+	public static final String X_Frame_Options = "X-Frame-Options";
+	/**
+	 * 跨站脚本攻击 (XSS)过滤器 X-XSS-Protection: 1; mode=block
+	 * 
+	 */
+	public static final String X_XSS_Protection = "X-XSS-Protection";
+	/**
+	 * 
+	 * 
+	 * 
+	 */
+	public static final String Content_Security_Policy = "Content-Security-Policy";
+	/**
+	 * 
+	 * 
+	 * 
+	 */
+	public static final String X_Content_Security_Policy = "X-Content-Security-Policy";
+	/**
+	 * 内容安全策略定义。 X-WebKit-CSP: default-src 'self'
+	 * 
+	 */
+	public static final String X_WebKit_CSP = "X-WebKit-CSP";
+	/**
+	 * The only defined value, "nosniff", prevents Internet Explorer from
+	 * MIME-sniffing a response away from the declared content-type. This also
+	 * applies to Google Chrome, when downloading extensions.[37]
+	 * X-Content-Type-Options: nosniff
+	 */
+	public static final String X_Content_Type_Options = "X-Content-Type-Options";
+	/**
+	 * 表明用于支持当前网页应用程序的技术（例如PHP）（版本号细节通常放置在 X-Runtime 或 X-Version 中）
+	 * X-Powered-By: PHP/5.4.0
+	 */
+	public static final String X_Powered_By = "X-Powered-By";
+	/**
+	 * Recommends the preferred rendering engine (often a backward-compatibility
+	 * mode) to use to display the content. Also used to activate Chrome Frame
+	 * in Internet Explorer. X-UA-Compatible: IE=EmulateIE7 X-UA-Compatible:
+	 * IE=edge X-UA-Compatible: Chrome=1
+	 */
+	public static final String X_UA_Compatible = "X-UA-Compatible";
+	/**
+	 * Provide the duration of the audio or video in seconds; only supported by
+	 * Gecko browsers X-Content-Duration: 42.666
+	 * 
+	 */
+	public static final String X_Content_Duration = "X-Content-Duration";
+
+	public void setResponseHead(String key, String value);
+
 	public void setResponseHeads(Map<String, String> heads);
 
 	public void setHttpVersion(String version);
+
 	public void setStatusCode(int code);
+
 	public void setContentType(String type);
+
 	public void setContentLength(long lenght);
-	
-	public void write(byte[] data,int off,int length);
+
+	public void write(byte[] data, int off, int length);
+
 	public void write(String text);
+
 	public void write(File file);
-	
-	public void serialize(OutputStream outputStream)throws IOException ;
-	
+
+	public void serialize(OutputStream outputStream) throws IOException;
+
 	public void addCookie(Cookie cookie);
 }
